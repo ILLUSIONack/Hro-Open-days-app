eebbbca71030f76777b1ae985998378efe659714
Furkan <0928212@hr.nl>
2019-03-29 12:09:45 +0100

    Date and Imageview

--- app/src/main/java/project/pb/fragments/HomeScreenFragment.java
+++ app/src/main/java/project/pb/fragments/HomeScreenFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 import project.pb.R;
 
@@ -16,6 +20,12 @@ public class HomeScreenFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_layout, container, false);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView textViewDate = view.findViewById(R.id.text_view_date);
+        textViewDate.setText(currentDate);
         return view;
     }
 }
