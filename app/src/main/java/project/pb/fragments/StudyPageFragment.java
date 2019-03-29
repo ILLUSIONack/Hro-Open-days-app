019fb768060a9dbd80f442e9bab86bda301783d4
Hadi S <hadi.zebeleh@gmail.com>
2019-03-29 12:25:41 +0100

    Made the buttons clickable in the course selection page

--- app/src/main/java/project/pb/fragments/StudyPageFragment.java
+++ app/src/main/java/project/pb/fragments/StudyPageFragment.java
@@ -1,5 +1,6 @@
 package project.pb.fragments;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,15 +8,65 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import project.pb.R;
 
 public class StudyPageFragment extends Fragment {
 
+    private ImageButton VoltijdInfButton;
+    private ImageButton VoltijdTechInfButton;
+    private ImageButton VoltijdCommButton;
+    private ImageButton VoltijdCmdButton;
+    private ImageButton DeeltijdInfButton;
+    private ImageButton DeeltijdCommButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_courseselection, container, false);
+        VoltijdInfButton = view.findViewById(R.id.InfKeuzeImageButton);
+        VoltijdInfButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        VoltijdTechInfButton = view.findViewById(R.id.TechInformKeuzeImageButton);
+        VoltijdTechInfButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        VoltijdCommButton = view.findViewById(R.id.CommunicatieKeuzeImageButton);
+        VoltijdCommButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        VoltijdCmdButton = view.findViewById(R.id.CmdKeuzeImageButton);
+        VoltijdCmdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        DeeltijdInfButton = view.findViewById(R.id.DeeltijdInfKeuzeButton);
+        DeeltijdInfButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        DeeltijdCommButton = view.findViewById(R.id.DeeltijdCommKeuzeButton);
+        DeeltijdCommButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return view;
 
     }
