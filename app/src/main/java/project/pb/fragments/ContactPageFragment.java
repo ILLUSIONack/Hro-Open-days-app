794f4f4894715e4c5b1306e53e6d6a29083a1d80
BlackReaper5 <amaterasu666111@hotmail.com>
2019-04-01 13:05:49 +0200

    Test for hadi

--- app/src/main/java/project/pb/fragments/ContactPageFragment.java
+++ app/src/main/java/project/pb/fragments/ContactPageFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import project.pb.R;
 
@@ -17,5 +18,7 @@ public class ContactPageFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.contact_page, container, false);
         return view;
+
     }
+
 }
