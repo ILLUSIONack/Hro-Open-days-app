71e0fca864e2d2e19e8961b9aa641089803cde4e
HamzDev <0963707@hr.nl>
2019-05-05 22:57:35 +0200

    Open dag information (first, second parts)

--- app/src/main/java/project/pb/opendag/OpenDagData.java
+++ app/src/main/java/project/pb/opendag/OpenDagData.java
@@ -6,7 +6,22 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "Informatica Voltijd",
+                    "Tijdens de informatica voltijd open dag zal je kennis maken",
+                    "met niet alleen de sfeer van de hogeschool rotterdam maar ook",
+                    "met een van de belangerijkste opleidingen in de ICT industrie",
+                    "namelijk informatica.",
+                    "De open dag informatica voltijd, zal eerst beginnen met een rondleiding",
+                    "rond het gebouw, hierdoor kom je precies te weten waar alles ligt.",
+                    "na de rondleiding wordt er een kleine cursus gegeven door een aantal studenten",
+                    "hierbij word een snelle uitleg gegeven over wat je kan verwachten.",
+                    "Nadat de curses voorbij is, krijg je nog kans om eventuelle vragen te stellem",
+                    "Hieronder kan je nog de planning van de dag bekijken",
+                    "",
+                    "De planning (4 April 16:00 t/m 20:00):",
+                    "- Rondleiding rond het gebouw",
+                    "- Cursus over informatica",
+                    "- Vragen over de cursus",
+                    "- Eventuelle inschrijvingen voor geintresseerde"
             };
         }
     },
@@ -14,7 +29,24 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "Technische informatica voltijd",
+                    "Tijdens de technische informatica voltijd open dag zal je kennis maken",
+                    "met niet alleen de sfeer van de hogeschool rotterdam maar ook",
+                    "met een van de belangerijkste opleidingen in de ICT industrie",
+                    "namelijk technische informatica.",
+                    "De open dag technische informatica voltijd, zal eerst beginnen met een rondleiding",
+                    "rond het gebouw, hierdoor kom je precies te weten waar alles ligt.",
+                    "na de rondleiding wordt er een kleine cursus gegeven door een aantal studenten",
+                    "hierbij word een snelle uitleg gegeven over wat je kan verwachten.",
+                    "Nadat de curses voorbij is, krijg je nog kans om eventuelle vragen te stellem",
+                    "Hieronder kan je nog de planning van de dag bekijken",
+                    "De dag zal eindige met een eindspel, het eindspel zal verschillende",
+                    "moeilijke puzzels bevatten die zo snel mogelijk moeten worden opgelost,",
+                    "het groepje dat het eindspel wint zal natuurlijk worden beloond met een prijs!",
+                    "",
+                    "De planning (4 April 16:00 t/m 20:00):",
+                    "- Rondleiding rond het gebouw",
+                    "- Cursus over technische informatica",
+                    "- Eindspel"
             };
         }
     },
