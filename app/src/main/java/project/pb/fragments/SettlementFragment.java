f80f49ef838eb2442eef17fc33a3fb598dfafcd2
BlackReaper5 <amaterasu666111@hotmail.com>
2019-05-06 20:12:56 +0200

    Button images

--- app/src/main/java/project/pb/fragments/SettlementFragment.java
+++ app/src/main/java/project/pb/fragments/SettlementFragment.java
@@ -7,19 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
-
+import android.net.Uri;
+import android.content.Intent;
 import project.pb.R;
 
 public class SettlementFragment extends Fragment {
-
-    private ImageView imageView4;
-
+    private Button link;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settlement_fragment, container, false);
+        link = view.findViewById(R.id.button);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://imgur.com/a/x5w4oGh"));
+                startActivity(browserIntent);
+            }
+
+        });
 
         return view;
+
     }
+
+
+
 }
