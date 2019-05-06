8532ec61899496343ce8c64c79a84d84a00e8a1d
HamzDev <0963707@hr.nl>
2019-05-06 18:05:53 +0200

    Study information english translation

--- app/src/main/java/project/pb/study/StudyData.java
+++ app/src/main/java/project/pb/study/StudyData.java
@@ -13,13 +13,13 @@ public enum StudyData implements StudyListener {
         @Override
         public String[] getGeneralInformation() {
             return new String[] {
-                    "We leiden je op tot software engineer. Je gaat aan de slag met het ontwikkelen van software voor diverse toepassingen. Je bent in staat complexe ICT-systemen te analyseren, ontwerpen en implementeren.",
-                    "          ",
-                    "Software is overal om ons heen te vinden. Jij bent de specialist in het maken van grote en complexe software informatiesystemen die snel, efficiënt en veilig werken. Zo kun je denken aan het ontwikkelen van een functionele app, maar ook aan het analyseren van grote hoeveelheden data van social media. Bij het ontwikkelen van deze systemen kom je in aanraking met verschillende programmeertalen. Zo leer je in het eerste jaar programmeren in Python en geavanceerde programmeertalen zoals Java/C#. Vanaf het tweede jaar kies je zelf welke programmeertaal je wilt gebruiken. Naast het programmeren en ontwikkelen van verschillende toepassingen leer je ook samenwerken.",
-                    "          ",
-                    "Na het volgen van de opleiding Informatica ben je breed inzetbaar binnen het vakgebied. Vanuit functies als data engineer, software developer en software engineer kun je verder groeien binnen de ICT.",
-                    "          ",
-                    "Ben jij geïnteresseerd in nieuwe technologische ontwikkelingen, nieuwsgierig, een beetje eigenzinnig en een doorzetter, dan is de opleiding Informatica iets voor jou!"
+                    "We train you to be a software engineer. You will work on developing software for various applications. You are able to analyze, design and implement complex IT systems.",
+                    "",
+                    "Software can be found all around us. You are the specialist in creating large and complex software information systems that work quickly, efficiently and securely. You can think of developing a functional app, but also analyzing large Amounts of data from social media When developing these systems you come into contact with different programming languages, for example in the first year you learn to program in Python and advanced programming languages ​​such as Java / C # From the second year you choose which programming language you want to use In addition to programming and developing various applications, you also learn to work together. ",
+                    "",
+                    "After following the Computer Science course, you can be widely deployed within the field. From positions such as data engineer, software developer and software engineer you can grow further within IT.",
+                    "",
+                    "If you are interested in new technological developments, curious, a little self-willed and a go-getter, then the Computer Science program is for you!"
             };
         }
     },
@@ -32,11 +32,11 @@ public enum StudyData implements StudyListener {
         @Override
         public String[] getGeneralInformation() {
             return new String[] {
-                    "Als technisch informaticus werk je aan moderne ICT-systemen waarbij hard- en software geïntegreerd zijn. Dankzij je kennis van programmeren en hardware kun je juist op het snijpunt daarvan opereren.",
-                    "          ",
-                    "In het dagelijks leven zijn we steeds meer afhankelijk geworden van ICT-systemen. Reizen in het OV kan alleen nog met de OV-chipkaart, je bagage komt op Schiphol automatisch op de juiste plek, energiemeters worden ‘slim’ en medische ingrepen worden soms door een apparaat in plaats van een chirurg gedaan. Allemaal voorbeelden waarbij hard- en software samenkomen in een enkel systeem.",
-                    "          ",
-                    "Als technisch informaticus draag je bij aan het ontwikkelen, beveiligen en in de lucht houden van deze systemen. Gedurende de studie maak je kennis met de technische aspecten van ICT. Je leert te programmeren in diverse programmeertalen, waaronder de zogenaamde 'lagere' talen voor hardware toepassingen. Hiermee leer je oplossingen ontwikkelen voor verschillende apparaten, robots en andere moderne systemen. Je bent een all-rounder op alle disciplines, maar specialiseert je ook in een of meerdere aspecten in het werkveld. Denk daarbij aan Machine Learning, Augmented Reality, Automated Systems en uiteraard de met IoT (Internet of Things) nauw verbonden met Embedded Systems."
+                    "As a technical computer scientist you work on modern IT systems where hardware and software are integrated. Thanks to your knowledge of programming and hardware, you can operate at the intersection thereof.",
+                    "",
+                    "In daily life we ​​have become increasingly dependent on IT systems. Traveling in public transport is only possible with the public transport chip card, your luggage will automatically arrive at the right place at Schiphol, energy meters will become 'smart' and medical interventions will sometimes become done by a device instead of a surgeon. All examples where hardware and software come together in a single system. ",
+                    "",
+                    "As a technical computer scientist you contribute to developing, securing and keeping these systems in the air. During the study you become acquainted with the technical aspects of ICT. You learn to program in various programming languages, including the so-called 'lower' languages ​​for You will learn how to develop solutions for different devices, robots and other modern systems.You are an all-rounder in all disciplines, but also specialize in one or more aspects in the field, such as Machine Learning, Augmented Reality, Automated Systems and of course the with IoT (Internet of Things) closely related to Embedded Systems. "
             };
         }
     }, VOLTIJD_COMMUNICATIE() {
@@ -48,15 +48,15 @@ public enum StudyData implements StudyListener {
         @Override
         public String[] getGeneralInformation() {
             return new String[] {
-                    "Heb je een scherp oog en oor voor de verhalen van mensen en merken? Ben je creatief met tekst en beeld en gefascineerd door de wereld van media? Dan is de opleiding Communicatie iets voor jou!",
-                    "          ",
-                    "Organisaties staan voor grote uitdagingen. Een nieuw product promoten, samenwerken met klanten, medewerkers en inwoners, laten zien dat je goede dingen doet voor de samenleving. Hoe doe je dat in een tijd van kritische journalisten, vloggers, maatschappelijke discussies en elkaar snel opvolgende trends?",
-                    "          ",
-                    "Onze opleiding Communicatie leidt jou in vier jaar op tot wat we - met trots - een smart connector noemen. Een professional die voor slimme verbindingen zorgt. Met inzet van bijvoorbeeld social tools, storytelling, big data en co-creatie. Je ontwikkelt creatieve concepten zoals mediacampagnes, events en verhalenplatforms. Op basis van goed onderzoek, theorie en strategie en natuurlijk jouw eigen verrassende ideeën.",
-                    "          ",
-                    "Vanaf jaar 1 werk je samen met opdrachtgevers uit de praktijk. Van Rotterdamse ondernemers, start-ups en bureaus tot landelijke organisaties. We werken met innovatieve methodes. In teamverband is dat bijvoorbeeld de scrum methodiek: waarbij je fl exibel en intensief samenwerkt met een opdrachtgever. Geen les, cursus of project is hetzelfde. We stemmen de werkvorm af op wat je leert: webinars, TED Talks, content cafés, meet-ups, gastcolleges en bedrijfsbezoeken.",
-                    "          ",
-                    "De opleiding Communicatie leert je om als smart connector mensen te raken, te verbinden en te bewegen."
+                    "Do you have a keen eye and ear for the stories of people and brands? Are you creative with text and image and fascinated by the world of media? Then the Communication course is for you!",
+                    "",
+                    "Organizations face major challenges. Promoting a new product, collaborating with customers, employees and residents, showing that you are doing good things for society. How do you do that in a time of critical journalists, vloggers, social discussions and rapid succession trends? ",
+                    "",
+                    "Our Communication course trains you in four years to what we - proudly - call a smart connector. A professional who ensures smart connections. With the use of social tools, storytelling, big data and co-creation, for example. You develop creative concepts such as media campaigns, events and story platforms. Based on good research, theory and strategy and of course your own surprising ideas. ",
+                    "",
+                    "From year 1 you have been working with clients from the field. From Rotterdam entrepreneurs, start-ups and agencies to national organizations. We work with innovative methods. In a team context that is, for example, the scrum method: where you work flexibly and intensively with a client. No lesson, course or project is the same. We tailor the teaching method to what you learn: webinars, TED Talks, content cafés, meet-ups, guest lectures and company visits. ",
+                    "",
+                    "The Communication course teaches you to touch, connect and move people as a smart connector."
             };
         }
 
@@ -68,12 +68,12 @@ public enum StudyData implements StudyListener {
 
         @Override
         public String[] getGeneralInformation() {
-            return new String[] {
-                    "Als CMD’er ontwerp je interactieve producten en diensten. Dat doe je met een creatief team voor échte opdrachtgevers in Studio’s en Labs.",
-                    "          ",
-                    "De Rotterdamse opleiding Communication and Multimedia Design is mensgericht. Dat betekent dat tijdens het ontwerpproces van bijvoorbeeld webapplicaties, informatiezuilen en interactieve installaties de gebruiker voortdurend centraal staat. Voordat je begint met een ontwerp doe je uitgebreid onderzoek naar de gebruikersbehoeften en naar de omgeving waarin jouw interactieve ontwerp wordt toegepast. Tijdens het ontwikkelproces controleer je voortdurend of je voor je gebruiker ontwerpt. Ontwerpen is immers: testen, falen, herontwerpen, testen, bijstellen etc. Naast nieuwsgierigheid en creativiteit, moet je als CMD’er dus een flinke portie doorzettings- en incasseringsvermogen hebben.",
-                    "          ",
-                    "In Studio’s en Labs werk je aan échte opdrachten. Onderdeel van het ontwerpproces is het bouwen van een prototype. Zo werkten eerstejaars aan de vraag hoe Lowlands de bezoekers van het festival gedurende het gehele jaar betrokken kon houden. De opleiding CMD daagt je uit jouw kennis, creativiteit en ontwerptalent in te zetten voor interactieve producten en diensten, die het leven en de omgeving van gebruikers veraangenamen of zelfs flink kunnen verbeteren."
+            return new String[]{
+                    "As a CMD employee you design interactive products and services. You do that with a creative team for real clients in Studios and Labs.",
+                    "",
+                    "The Rotterdam study program Communication and Multimedia Design is people-oriented. That means that during the design process of, for example, web applications, information kiosks and interactive installations, the user is constantly at the center. Before you start a design, you do extensive research into user needs and the environment in which During the development process you constantly check whether you are designing for your user, since designing is: testing, failure, redesigning, testing, adjusting, etc. In addition to curiosity and creativity, you, as a CMD person, also need a large portion of perseverance. and have capacity to collect. ",
+                    "",
+                    "In Studios and Labs you work on real assignments. Part of the design process is building a prototype. First-year students worked on the question of how Lowlands could keep visitors to the festival involved throughout the year. The CMD course challenges you use knowledge, creativity and design talent for interactive products and services that make life and environment of users more pleasant or even considerably improve. "
             };
         }
 
@@ -85,12 +85,12 @@ public enum StudyData implements StudyListener {
 
         @Override
         public String[] getGeneralInformation() {
-            return new String[] {
-                    "Ben jij toe aan de volgende stap in je loopbaan waarbij je je onder andere bezig houdt met softwaresystemen, informatiesystemen of telematicatoepassingen?",
-                    "          ",
-                    "ICT is overal om ons heen te vinden. Jij bent als informaticus de specialist in het maken van grote en complexe informatiesystemen die snel, efficiënt en veilig werken. Dit varieert van webshops en apps tot big data analyses van social media. Bij het ontwikkelen van deze systemen kom je in aanraking met verschillende programmeertalen als: JavaScript, HTML5, CSS3, Java (Object georiënteerd programmeren), vanaf het tweede jaar is hierin meer variatie. Naast het programmeren en ontwikkelen van verschillende toepassingen leer je ook samenwerken met medestudenten of met studenten van andere opleidingen.",
-                    "          ",
-                    "De deeltijdopleiding Informatica is bedoeld voor mensen die toe zijn aan een volgende stap in hun loopbaan. Tijdens je studie is er aandacht voor je persoonlijke en beroepsontwikkeling. Je krijgt de hulp en begeleiding waaraan je zelf behoefte hebt; zo geef je zelf sturing aan je eigen studieloopbaan en beroepscarrière. Ben jij nieuwsgierig, een puzzelaar, een beetje eigenzinnig, een doorzetter, geïnteresseerd in nieuwe mogelijkheden en/of toepassingen en werk je graag met en voor mensen? Dan is Informatica iets voor jou!",
+            return new String[]{
+                    "Are you ready for the next step in your career where you are involved in software systems, information systems or telematics applications?",
+                    "",
+                    "IT can be found all around us. As a computer scientist you are the specialist in creating large and complex information systems that work quickly, efficiently and securely. This varies from web shops and apps to big data analyzes of social media. When developing these systems come into contact with different programming languages ​​such as: JavaScript, HTML5, CSS3, Java (object-oriented programming), there is more variation in this from the second year onwards. from other courses. ",
+                    "",
+                    "The part-time Computer Science course is intended for people who are ready for the next step in their career. During your studies attention is paid to your personal and professional development. You get the help and guidance you need yourself, so you manage your own your own study career and professional career. Are you curious, a puzzler, a little self-willed, a go-getter, interested in new possibilities and / or applications and do you enjoy working with and for people? Then Informatica is for you! ",
             };
         }
     }, DEELTIJD_COMMUNICATIE() {
@@ -101,19 +101,19 @@ public enum StudyData implements StudyListener {
 
         @Override
         public String[] getGeneralInformation() {
-            return new String[] {
-                    "Je wilt vanuit je huidige functie als marketingmedewerker of communicatieadviseur graag doorgroeien naar een managementfunctie, maar daarvoor wordt wel een bachelordiploma gevraagd.",
-                    "          ",
-                    "Het is ook mogelijk dat je al een hbo-opleiding achter de rug hebt, maar dat je van beroep wilt veranderen of  je hebt eerder een economische hbo-opleiding niet afgemaakt en wilt nu toch het bijbehorende diploma - én de carrièreperspectieven - verwerven.",
-                    "          ",
-                    "Een deeltijdopleiding van Hogeschool Rotterdam is in al die gevallen een efficiënte weg naar het diploma. Bij Hogeschool Rotterdam is de deeltijdopleiding Communicatie verdeeld in twee overzichtelijke periodes van twee jaar, die je beide afsluit met een volwaardig diploma:",
-                    "          ",
-                    "1:de deeltijd Associate degree Crossmediale Communicatie (jaar 1 en 2)",
-                    "en 2:de deeltijd bachelor Communicatie (jaar 3 en 4)",
-                    "          ",
-                    "De opleiding Communicatie is innovatief en praktijkgericht en leidt je op tot ‘smart connector’. Een smart connector is een professional die mensen slim met elkaar verbindt met de inzet van bijvoorbeeld social tools, big data en co-creatie.",
-                    "          ",
-                    "De opleiding biedt je alle ‘tools’ om als ‘smart connector’ mensen te kunnen raken, verbinden en bewegen. Op het goede moment, via het juiste communicatiemiddel en met inzet van nieuwe technologie!"
+            return new String[]{
+                    "From your current position as a marketing employee or communication consultant, you would like to grow into a management position, but you will need a bachelor's degree for this.",
+                    "",
+                    "It is also possible that you have already completed a higher professional education, but that you want to change your profession or you have not previously completed an economic higher professional education and now want to acquire the corresponding diploma - and career prospects.",
+                    "",
+                    "A part-time course at Rotterdam University of Applied Sciences is an efficient way to get a diploma in all these cases. At Rotterdam University of Applied Sciences, the part-time course in Communication is divided into two clear two-year periods, which you both complete with a fully-fledged diploma:",
+                    "",
+                    "1: the part-time Associate degree Cross-media Communication (year 1 and 2)",
+                    "and 2: the part-time Bachelor's degree in Communication (years 3 and 4)",
+                    "",
+                    "The Communication course is innovative and practice-oriented and trains you to become a smart connector. A smart connector is a professional who connects people smartly with the use of, for example, social tools, big data and co-creation.",
+                    "",
+                    "The training offers you all the tools to be able to touch, connect and move people as a smart connector. At the right time, via the right means of communication and with the use of new technology!"
             };
         }
     }
