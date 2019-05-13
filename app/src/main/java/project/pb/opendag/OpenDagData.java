060d57c30f87b9ff4d8ec816441349272bbc5fbd
HadiDragneel <47558826+hadidragneel@users.noreply.github.com>
2019-05-13 19:30:35 +0200

    Fixed text displays

--- app/src/main/java/project/pb/opendag/OpenDagData.java
+++ app/src/main/java/project/pb/opendag/OpenDagData.java
@@ -44,17 +44,12 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "During the informatica voltijd open day you will learn more",
-                    "about not just the atmosphere of the Hogeschool Rotterdam, but",
-                    "also with one of the most important studies in the ICT industry",
-                    "The open day computer science full-time, will first start with a tour",
-                    "around the building, this way you can find out exactly where everything is.",
-                    "after the tour a small course is given by a number of students",
-                    "Hereby a quick explanation is given about what you can expect.",
-                    "After the courses are over, you still have a chance to ask any questions",
-                    "Below you can view the schedule for the day",
+                    "Program:",
+                    "",
+                    "During the informatica voltijd open day you will learn more about not just the atmosphere of the Hogeschool Rotterdam, but also with one of the most important studies in the ICT industry. The open day computer science full-time, will first start with a tour around the building, this way you can find out exactly where everything is. After the tour a small course is given by a number of students. Hereby a quick explanation is given about what you can expect. After the courses are over, you still have a chance to ask any questions. Below you can view the schedule for the day",
                     "",
                     "The schedule (April 4 from 4:00 pm to 8:00 pm):",
+                    "",
                     "- Tour around the building",
                     "- Computer science course",
                     "- Questions about the course",
@@ -102,21 +97,12 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "You will become acquainted during technical computer science full-time open day",
-                    "with not only the atmosphere of the Rotterdam university but also",
-                    "with one of the most important courses in the IT industry",
-                    "namely technical informatics.",
-                    "The open day technical computer science full time, will first start with a tour",
-                    "around the building, this way you can find out exactly where everything is.",
-                    "after the tour a small course is given by a number of students",
-                    "Hereby a quick explanation is given about what you can expect.",
-                    "After the courses are over, you still have a chance to ask any questions",
-                    "Below you can view the schedule for the day",
-                    "The day will end with an endgame, the endgame will be different",
-                    "contain difficult puzzles that must be solved as quickly as possible,",
-                    "the group that wins the endgame will of course be rewarded with a prize!",
+                    "Program:",
+                    "",
+                    "You will become acquainted during technical computer science full-time open day with not only the atmosphere of the Rotterdam university but also with one of the most important courses in the IT industry: technical informatics. The open day technical computer science full time, will first start with a tour around the building. This way you can find out exactly where everything is. After the tour a small course is given by a number of students. Hereby a quick explanation is given about what you can expect. After the courses are over, you still have a chance to ask any questions. Below you can view the schedule for the day. The day will end with an endgame, the endgame will be different, and contains difficult puzzles that must be solved as quickly as possible. The group that wins the endgame will of course be rewarded with a prize!",
                     "",
                     "The schedule (April 4 from 4:00 pm to 8:00 pm):",
+                    "",
                     "- Tour around the building",
                     "- Course on technical informatics",
                     "- Endgame"
@@ -163,17 +149,14 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "The open day for Communication and Multimedia design full time will start with giving you",
-                    "a taste of the atmosphere of the school and the study.",
-                    "You will be getting a comfortable experience of what the study will be offering you.",
-                    "At first the employees and students will show you the interior of the school",
-                    "Than a small course wil take place given by students which will create an",
-                    "opportunity to ask the students how their experience is of the study.",
-                    "The day will end with a game which consists of difficult puzzles.",
-                    "And of course the winning team will earn a price.\n",
-                    "The planning(5 April 16:00 t/m 20:00):\n",
-                    "- Tour of the building",
-                    "- Course about Communication and Multimedia design",
+                    "Program:",
+                    "",
+                    "You will become acquainted during communication and media full-time open day with not only the atmosphere of the Rotterdam university but also with one of the most important courses in the IT industry: communication and media. The open day communication and media full time, will first start with a tour around the building. This way you can find out exactly where everything is. After the tour a small course is given by a number of students. Hereby a quick explanation is given about what you can expect. After the courses are over, you still have a chance to ask any questions. Below you can view the schedule for the day. The day will end with an endgame, the endgame will be different, and contains difficult puzzles that must be solved as quickly as possible. The group that wins the endgame will of course be rewarded with a prize!",
+                    "",
+                    "The schedule (April 4 from 4:00 pm to 8:00 pm):",
+                    "",
+                    "- Tour around the building",
+                    "- Course on technical informatics",
                     "- Endgame"
             };
         }
@@ -217,17 +200,14 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "The open day for Communication full time will start with giving you",
-                    "a taste of the atmosphere of the school and the study.",
-                    "You will be getting a comfortable experience of what the study will be offering you.",
-                    "At first the employees and students will show you the interior of the school",
-                    "Than a small course wil take place given by students which will create an",
-                    "opportunity to ask the students how their experience is of the study.",
-                    "The day will end with a game which consists of difficult puzzles.",
-                    "And of course the winning team will earn a price.\n",
-                    "The planning(5 April 16:00 t/m 20:00):\n",
-                    "- Tour of the building",
-                    "- Course about Communication",
+                    "Program:",
+                    "",
+                    "You will become acquainted during communication full-time open day with not only the atmosphere of the Rotterdam university but also with one of the most important courses in the IT industry: communication. The open day communication full time, will first start with a tour around the building. This way you can find out exactly where everything is. After the tour a small course is given by a number of students. Hereby a quick explanation is given about what you can expect. After the courses are over, you still have a chance to ask any questions. Below you can view the schedule for the day. The day will end with an endgame, the endgame will be different, and contains difficult puzzles that must be solved as quickly as possible. The group that wins the endgame will of course be rewarded with a prize!",
+                    "",
+                    "The schedule (April 4 from 4:00 pm to 8:00 pm):",
+                    "",
+                    "- Tour around the building",
+                    "- Course on technical informatics",
                     "- Endgame"
             };
         }
@@ -272,17 +252,14 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "The open day for part time Informatica will start with giving you",
-                    "a taste of the atmosphere of the school and the study.",
-                    "You will be getting a comfortable experience of what the study will be offering you.",
-                    "At first the employees and students will show you the interior of the school",
-                    "Than a small course wil take place given by students which will create an",
-                    "opportunity to ask the students how their experience is of the study.",
-                    "The day will end with a game which consists of difficult puzzles.",
-                    "And of course the winning team will earn a price.\n",
-                    "The planning(5 April 16:00 t/m 20:00):\n",
-                    "- Tour of the building",
-                    "- Course about part time Informatica",
+                    "Program:",
+                    "",
+                    "You will become acquainted during informatics part-time open day with not only the atmosphere of the Rotterdam university but also with one of the most important courses in the IT industry: informatics. The open day informatics part-time, will first start with a tour around the building. This way you can find out exactly where everything is. After the tour a small course is given by a number of students. Hereby a quick explanation is given about what you can expect. After the courses are over, you still have a chance to ask any questions. Below you can view the schedule for the day. The day will end with an endgame, the endgame will be different, and contains difficult puzzles that must be solved as quickly as possible. The group that wins the endgame will of course be rewarded with a prize!",
+                    "",
+                    "The schedule (April 4 from 4:00 pm to 8:00 pm):",
+                    "",
+                    "- Tour around the building",
+                    "- Course on technical informatics",
                     "- Endgame"
             };
         }
@@ -327,17 +304,14 @@ public enum OpenDagData implements OpenDagListener {
         @Override
         public String[] getInformation() {
             return new String[] {
-                    "The open day for part time communication will start with giving you",
-                    "a taste of the atmosphere of the school and the study.",
-                    "You will be getting a comfortable experience of what the study will be offering you.",
-                    "At first the employees and students will show you the interior of the school",
-                    "Than a small course wil take place given by students which will create an",
-                    "opportunity to ask the students how their experience is of the study.",
-                    "The day will end with a game which consists of difficult puzzles.",
-                    "And of course the winning team will earn a price.\n",
-                    "The planning(5 April 16:00 t/m 20:00):\n",
-                    "- Tour of the building",
-                    "- Course about part time communication",
+                    "Program:",
+                    "",
+                    "You will become acquainted during communication part-time open day with not only the atmosphere of the Rotterdam university but also with one of the most important courses in the IT industry: communication. The open day communication part time, will first start with a tour around the building. This way you can find out exactly where everything is. After the tour a small course is given by a number of students. Hereby a quick explanation is given about what you can expect. After the courses are over, you still have a chance to ask any questions. Below you can view the schedule for the day. The day will end with an endgame, the endgame will be different, and contains difficult puzzles that must be solved as quickly as possible. The group that wins the endgame will of course be rewarded with a prize!",
+                    "",
+                    "The schedule (April 4 from 4:00 pm to 8:00 pm):",
+                    "",
+                    "- Tour around the building",
+                    "- Course on technical informatics",
                     "- Endgame"
             };
         }
