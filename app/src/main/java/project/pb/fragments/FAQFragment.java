12afb13291b02992f16ff31b0aab4353e8cf3a8c
ILLUSIONack <usman-ack@hotmail.com>
2019-05-23 06:15:35 +0200

    Added more questions to the FAQ page

--- app/src/main/java/project/pb/fragments/FAQFragment.java
+++ app/src/main/java/project/pb/fragments/FAQFragment.java
@@ -29,9 +29,9 @@ public class FAQFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.gestelde_vragen, container, false);
+        getActivity().setTitle("FAQ");
         initialisation(view);
         return view;
-
     }
 
 
@@ -43,30 +43,59 @@ public class FAQFragment extends Fragment {
         Questions.add("Where is the school located?");
         Questions.add("Is the food available at the Open day?");
         Questions.add("What time does the Open day usually end?");
+        Questions.add("Can i park outside the school?");
+        Questions.add("Where is the school located?");
+        Questions.add("Is the food available at the Open day?");
+        Questions.add("What time does the Open day usually end?");
+        Questions.add("Can i park outside the school?");
+        Questions.add("Where is the school located?");
+        Questions.add("Is the food available at the Open day?");
+        Questions.add("What time does the Open day usually end?");
 
         List<String> answer1 = new ArrayList<>();
         List<String> answer2 = new ArrayList<>();
         List<String> answer3 = new ArrayList<>();
         List<String> answer4 = new ArrayList<>();
+        List<String> answer5 = new ArrayList<>();
+        List<String> answer6 = new ArrayList<>();
+        List<String> answer7 = new ArrayList<>();
+        List<String> answer8 = new ArrayList<>();
+        List<String> answer9 = new ArrayList<>();
+        List<String> answer10= new ArrayList<>();
+        List<String> answer11 = new ArrayList<>();
+        List<String> answer12 = new ArrayList<>();
 
         answer1.add("Yes, you can find paid parking outside.");
         answer2.add("The school is located near the station Beurs");
         answer3.add("Yes, snacks and drinks are provided");
         answer4.add("Depends on the chosen study but usually at 15:00");
+        answer5.add("Yes, you can find paid parking outside.");
+        answer6.add("The school is located near the station Beurs");
+        answer7.add("Yes, snacks and drinks are provided");
+        answer8.add("Depends on the chosen study but usually at 15:00");
+        answer9.add("Yes, you can find paid parking outside.");
+        answer10.add("The school is located near the station Beurs");
+        answer11.add("Yes, snacks and drinks are provided");
+        answer12.add("Depends on the chosen study but usually at 15:00");
 
         answers.put(Questions.get(0),answer1);
         answers.put(Questions.get(1),answer2);
         answers.put(Questions.get(2),answer3);
         answers.put(Questions.get(3),answer4);
+        answers.put(Questions.get(4),answer5);
+        answers.put(Questions.get(5),answer6);
+        answers.put(Questions.get(6),answer7);
+        answers.put(Questions.get(7),answer8);
+        answers.put(Questions.get(8),answer9);
+        answers.put(Questions.get(9),answer10);
+        answers.put(Questions.get(10),answer11);
+        answers.put(Questions.get(11),answer12);
     }
 
 
     public void initialisation(View view){
-        getActivity().setTitle("FAQ");
-
         expandableListView = view.findViewById(R.id.eListView);
         fillData();
-
         listAdapter = new MyExListAdaptor(getContext(),Questions,answers);
         expandableListView.setAdapter(listAdapter);
     }
--- app/src/main/java/project/pb/fragments/OpenDayInformation.java
+++ app/src/main/java/project/pb/fragments/OpenDayInformation.java
@@ -8,10 +8,13 @@ import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -22,14 +25,16 @@ import java.util.List;
 
 import project.pb.R;
 import project.pb.opendag.OpenDagData;
-import project.pb.zoom.MultiTouchListener;
 
-public class OpenDayInformation extends AppCompatActivity {
+public class OpenDayInformation extends AppCompatActivity implements View.OnClickListener {
 
     private Button addcalender;
     private TextView generalInfo;
     private ImageButton shareButton;
-    private ConstraintLayout opendaypage;
+
+    private float mScale = 1f;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +42,6 @@ public class OpenDayInformation extends AppCompatActivity {
         setContentView(R.layout.activity_inf1);
         generalInfo = findViewById(R.id.textView3);
         addcalender = findViewById(R.id.addcalender);
-        opendaypage = findViewById(R.id.opendaypage);
 
 
         final OpenDagData key = (OpenDagData) getIntent().getSerializableExtra("open_dag_informatie");
@@ -78,7 +82,53 @@ public class OpenDayInformation extends AppCompatActivity {
             }
         });
 
-        generalInfo.setOnTouchListener(new MultiTouchListener());
+        gestureDetector = new GestureDetector(this, new GestureListener());
+
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener(){
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                float scale = 1 - detector.getScaleFactor();
+                float prevScale = mScale;
+                mScale += scale;
+
+                if (mScale > 1.1f) {
+                    mScale = 1.1f;
+                }
+                if (mScale < 0.4f) {
+                    mScale = 0.4f;
+                }
+                ScaleAnimation scaleAnimation = new ScaleAnimation(1f / prevScale, 1f / mScale, 1f / prevScale, 1f / mScale, detector.getFocusX(), detector.getFocusY());
+                scaleAnimation.setDuration(0);
+                scaleAnimation.setFillAfter(true);
+                generalInfo.startAnimation(scaleAnimation);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        super.dispatchTouchEvent(event);
+        mScaleGestureDetector.onTouchEvent(event);
+        gestureDetector.onTouchEvent(event);
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return true;
+        }
     }
 
     public void onShareClick(View v, OpenDagData key){
