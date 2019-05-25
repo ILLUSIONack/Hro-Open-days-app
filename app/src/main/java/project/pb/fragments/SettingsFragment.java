412e0056d176c23965db4f9deb534ef20958914d
Furkan <0928212@hr.nl>
2019-05-25 21:20:26 +0200

    Save mode state(not working atm)

--- app/src/main/java/project/pb/fragments/SettingsFragment.java
+++ app/src/main/java/project/pb/fragments/SettingsFragment.java
@@ -1,11 +1,9 @@
 package project.pb.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,30 +15,31 @@ import project.pb.R;
 public class SettingsFragment extends Fragment {
 
     private Switch myswitch;
-
+    SharedPref sharedPref;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_page, container, false);
         getActivity().setTitle("Settings");
 
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+        sharedPref = new SharedPref(this);
+        if (sharedPref.loadNightModeState()==true) {
             getActivity().setTheme(R.style.AppTheme);
         } else {
             getActivity().setTheme(R.style.DarkTheme);
         }
 
         myswitch = (Switch) view.findViewById(R.id.switch3);
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+        if (sharedPref.loadNightModeState()==true) {
             myswitch.setChecked(true);
         }
         myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    sharedPref.setNightModeState(true);
                     refreshApplciation();
                 } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    sharedPref.setNightModeState(false);
                     refreshApplciation();
                 }
             }
