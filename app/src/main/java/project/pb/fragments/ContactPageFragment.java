280a9277286f2554e948dbb3b64e3d2f8a5ce7b9
ILLUSIONack <usman-ack@hotmail.com>
2019-05-29 09:34:00 +0200

    I made a select email client instead of message sent, there is alrdy a sending message toast

--- app/src/main/java/project/pb/fragments/ContactPageFragment.java
+++ app/src/main/java/project/pb/fragments/ContactPageFragment.java
@@ -1,5 +1,6 @@
 package project.pb.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,10 +14,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import project.pb.R;
 
 public class ContactPageFragment extends Fragment implements View.OnClickListener {
+
+    private EditText your_name;
+    private EditText your_subject;
+    private EditText your_message;
+    private ImageView callImage;
+    private Button email;
+
 
     @Nullable
     @Override
@@ -28,52 +37,72 @@ public class ContactPageFragment extends Fragment implements View.OnClickListene
 
     private void initialize(View view) {
         getActivity().setTitle("Contact");
-        ImageView callImage = view.findViewById(R.id.phoneCallButtonImage);
-        Button email = view.findViewById(R.id.buttonSend);
+        your_name = view.findViewById(R.id.textName);
+        your_subject = view.findViewById(R.id.textSubject);
+        your_message = view.findViewById(R.id.textMessage);
+        callImage = view.findViewById(R.id.phoneCallButtonImage);
+        email = view.findViewById(R.id.buttonSend);
         callImage.setOnClickListener(this);
         email.setOnClickListener(this);
     }
 
+    public void phoneCall(){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:+3164333431"));
+        startActivity(callIntent);
+    }
+
+    //Asks the users to choose a email service of choice
+    public void chooseEmailService(){
+        Context context = getContext();
+        CharSequence text = "Please select Email Client";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void sendMessage(){
+        String name = your_name.getText().toString();
+        String subject = your_subject.getText().toString();
+        String message = your_message.getText().toString();
+        if (TextUtils.isEmpty(name)) {
+            your_name.setError("Enter Your Name");
+            your_name.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(subject)) {
+            your_subject.setError("Enter Your Subject");
+            your_subject.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(message)) {
+            your_message.setError("Enter Your Message");
+            your_message.requestFocus();
+            return;
+        }
+
+        Intent sendEmail = new Intent(android.content.Intent.ACTION_SEND);
+        sendEmail.setType("plain/text");
+        sendEmail.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"usman-ack@hotmail.com"});
+        sendEmail.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        sendEmail.putExtra(android.content.Intent.EXTRA_TEXT,
+                "name:" + name + '\n' + "Message:" + '\n' + message);
+
+        /* Send it off to the Activity-Chooser */
+        startActivity(Intent.createChooser(sendEmail, "Send mail..."));
+
+        chooseEmailService();
+    }
+
     @Override
     public void onClick(View v) {
-        final EditText your_name = v.findViewById(R.id.textName);
-        final EditText your_subject = v.findViewById(R.id.textSubject);
-        final EditText your_message = v.findViewById(R.id.textMessage);
         switch(v.getId()){
             case R.id.phoneCallButtonImage:
-                Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:+3164333431"));
-                startActivity(callIntent);
+                phoneCall();
                 break;
             case R.id.buttonSend:
-                String name = your_name.getText().toString();
-                String subject = your_subject.getText().toString();
-                String message = your_message.getText().toString();
-                if (TextUtils.isEmpty(name)) {
-                    your_name.setError("Enter Your Name");
-                    your_name.requestFocus();
-                    return;
-                }
-                if (TextUtils.isEmpty(subject)) {
-                    your_subject.setError("Enter Your Subject");
-                    your_subject.requestFocus();
-                    return;
-                }
-                if (TextUtils.isEmpty(message)) {
-                    your_message.setError("Enter Your Message");
-                    your_message.requestFocus();
-                    return;
-                }
-
-                Intent sendEmail = new Intent(android.content.Intent.ACTION_SEND);
-                sendEmail.setType("plain/text");
-                sendEmail.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"usman-ack@hotmail.com"});
-                sendEmail.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-                sendEmail.putExtra(android.content.Intent.EXTRA_TEXT,
-                        "name:" + name + '\n' + "Message:" + '\n' + message);
-
-                /* Send it off to the Activity-Chooser */
-                startActivity(Intent.createChooser(sendEmail, "Send mail..."));
+                sendMessage();
         }
     }
 }
