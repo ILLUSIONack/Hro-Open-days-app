8dab0d0cd35f7fc654e23855eaefdd4318a86023
HamzDev <0963707@hr.nl>
2019-05-29 15:41:39 +0200

    Added Tap Zoom For Images, with framelayout

--- app/src/main/java/project/pb/fragments/MapWijnhaven_107.java
+++ app/src/main/java/project/pb/fragments/MapWijnhaven_107.java
@@ -8,14 +8,10 @@ import project.pb.R;
 
 public class MapWijnhaven_107 extends Activity {
 
-    private ScrollView scrollViewMaps;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.floor_plan_107);
-        scrollViewMaps = findViewById(R.id.scrollViewMaps);
-        scrollViewMaps.setScrollbarFadingEnabled(false);
 
         //TODO THIS CAUSES THE SCROLLVIEW TO NOT SCROLL!!!!!!!!!!!!!!!!!!!!!!!!!!
         //scrollViewMaps.setOnTouchListener(new MultiTouchListener());
--- app/src/main/java/project/pb/fragments/MapWijnhaven_99.java
+++ app/src/main/java/project/pb/fragments/MapWijnhaven_99.java
@@ -1,27 +1,236 @@
 package project.pb.fragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 
 import project.pb.R;
 import project.pb.zoom.MultiTouchListener;
 
-public class MapWijnhaven_99 extends Activity {
+public class MapWijnhaven_99 extends FragmentActivity implements View.OnClickListener {
 
-    private ScrollView scrollViewMaps;
+    // Hold a reference to the current animator,
+    // so that it can be canceled mid-way.
+    private Animator currentAnimator;
+
+    // The system "short" animation time duration, in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // very frequently.
+    private int shortAnimationDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.floor_plan_99);
-        scrollViewMaps = findViewById(R.id.scrollViewMaps);
-        scrollViewMaps.setScrollbarFadingEnabled(false);
 
+        // Hook up clicks on the thumbnail views.
+
+        final View thumb1View = findViewById(R.id.thumb_button_1);
+        final View thumb2View = findViewById(R.id.thumb_button_2);
+        final View thumb3View = findViewById(R.id.thumb_button_3);
+        final View thumb4View = findViewById(R.id.thumb_button_4);
+        final View thumb5View = findViewById(R.id.thumb_button_5);
+        final View thumb6View = findViewById(R.id.thumb_button_6);
+        final View thumb7View = findViewById(R.id.thumb_button_7);
+        final View thumb8View = findViewById(R.id.thumb_button_8);
+        final View thumb9View = findViewById(R.id.thumb_button_9);
+        thumb1View.setOnClickListener(this);
+        thumb2View.setOnClickListener(this);
+        thumb3View.setOnClickListener(this);
+        thumb4View.setOnClickListener(this);
+        thumb5View.setOnClickListener(this);
+        thumb6View.setOnClickListener(this);
+        thumb7View.setOnClickListener(this);
+        thumb8View.setOnClickListener(this);
+        thumb9View.setOnClickListener(this);
+
+        // Retrieve and cache the system's default "short" animation time.
+        shortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
 
         //TODO THIS CAUSES THE SCROLLVIEW TO NOT SCROLL!!!!!!!!!!!!!!!!!!!!!!!!!!
         //scrollViewMaps.setOnTouchListener(new MultiTouchListener());
         //scrollViewMaps.setOnTouchListener(new MultiTouchListener());
+    }
+
+    private void zoomImageFromThumb(final View thumbView, int imageResId) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final ImageView expandedImageView = (ImageView) findViewById(R.id.expanded_image);
+        expandedImageView.setImageResource(imageResId);
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(1f);
+        expandedImageView.setPivotY(1f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f))
+                .with(ObjectAnimator.ofFloat(expandedImageView,
+                        View.SCALE_Y, startScale, 1f));
+        set.setDuration(shortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                currentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                currentAnimator = null;
+            }
+        });
+        set.start();
+        currentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentAnimator != null) {
+                    currentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y, startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(shortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        currentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        currentAnimator = null;
+                    }
+                });
+                set.start();
+                currentAnimator = set;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.thumb_button_1:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_min2);
+                break;
+            case R.id.thumb_button_2:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_min1);
+                break;
+            case R.id.thumb_button_3:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_1);
+                break;
+            case R.id.thumb_button_4:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_2);
+                break;
+            case R.id.thumb_button_5:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_3);
+                break;
+            case R.id.thumb_button_6:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_4);
+                break;
+            case R.id.thumb_button_7:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_5);
+                break;
+            case R.id.thumb_button_8:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_bg);
+                break;
+            case R.id.thumb_button_9:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_99_dak);
+                break;
+        }
     }
 }
 
