8f6cd1bf2bf19ed19ae05afa9e825bb3b646e0c8
BlackReaper5 <amaterasu666111@hotmail.com>
2019-05-30 09:47:28 +0200

    Made the buttons for Wijnhaven 103 and 107. Still need to give proper titles for each floor.

--- app/src/main/java/project/pb/fragments/MapWijnhaven_103.java
+++ app/src/main/java/project/pb/fragments/MapWijnhaven_103.java
@@ -26,6 +26,27 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.floor_plan_103);
+
+        final View thumb21View = findViewById(R.id.thumb_button_21);
+        final View thumb22View = findViewById(R.id.thumb_button_22);
+        final View thumb23View = findViewById(R.id.thumb_button_23);
+        final View thumb24View = findViewById(R.id.thumb_button_24);
+        final View thumb25View = findViewById(R.id.thumb_button_25);
+        final View thumb26View = findViewById(R.id.thumb_button_26);
+        final View thumb27View = findViewById(R.id.thumb_button_27);
+        final View thumb28View = findViewById(R.id.thumb_button_28);
+        final View thumb29View = findViewById(R.id.thumb_button_29);
+
+        thumb21View.setOnClickListener(this);
+        thumb22View.setOnClickListener(this);
+        thumb23View.setOnClickListener(this);
+        thumb24View.setOnClickListener(this);
+        thumb25View.setOnClickListener(this);
+        thumb26View.setOnClickListener(this);
+        thumb27View.setOnClickListener(this);
+        thumb28View.setOnClickListener(this);
+        thumb29View.setOnClickListener(this);
+        
         shortAnimationDuration = getResources().getInteger(
                 android.R.integer.config_shortAnimTime);
     }
@@ -35,8 +56,8 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
             currentAnimator.cancel();
         }
 
-        final ImageView expandedImageView = (ImageView) findViewById(R.id.expanded_image);
-        expandedImageView.setImageResource(imageResId);
+        final ImageView expandedImageView2 = (ImageView) findViewById(R.id.expanded_image2);
+        expandedImageView2.setImageResource(imageResId);
 
         final Rect startBounds = new Rect();
         final Rect finalBounds = new Rect();
@@ -66,20 +87,20 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
         }
 
         thumbView.setAlpha(0f);
-        expandedImageView.setVisibility(View.VISIBLE);
+        expandedImageView2.setVisibility(View.VISIBLE);
 
-        expandedImageView.setPivotX(1f);
-        expandedImageView.setPivotY(1f);
+        expandedImageView2.setPivotX(1f);
+        expandedImageView2.setPivotY(1f);
 
         AnimatorSet set = new AnimatorSet();
         set
-                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                .play(ObjectAnimator.ofFloat(expandedImageView2, View.X,
                         startBounds.left, finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                .with(ObjectAnimator.ofFloat(expandedImageView2, View.Y,
                         startBounds.top, finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                .with(ObjectAnimator.ofFloat(expandedImageView2, View.SCALE_X,
                         startScale, 1f))
-                .with(ObjectAnimator.ofFloat(expandedImageView,
+                .with(ObjectAnimator.ofFloat(expandedImageView2,
                         View.SCALE_Y, startScale, 1f));
         set.setDuration(shortAnimationDuration);
         set.setInterpolator(new DecelerateInterpolator());
@@ -99,7 +120,7 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
 
 
         final float startScaleFinal = startScale;
-        expandedImageView.setOnClickListener(new View.OnClickListener() {
+        expandedImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (currentAnimator != null) {
@@ -110,15 +131,15 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
                 // back to their original values.
                 AnimatorSet set = new AnimatorSet();
                 set.play(ObjectAnimator
-                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .ofFloat(expandedImageView2, View.X, startBounds.left))
                         .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
+                                .ofFloat(expandedImageView2,
                                         View.Y, startBounds.top))
                         .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
+                                .ofFloat(expandedImageView2,
                                         View.SCALE_X, startScaleFinal))
                         .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
+                                .ofFloat(expandedImageView2,
                                         View.SCALE_Y, startScaleFinal));
                 set.setDuration(shortAnimationDuration);
                 set.setInterpolator(new DecelerateInterpolator());
@@ -126,14 +147,14 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
+                        expandedImageView2.setVisibility(View.GONE);
                         currentAnimator = null;
                     }
 
                     @Override
                     public void onAnimationCancel(Animator animation) {
                         thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
+                        expandedImageView2.setVisibility(View.GONE);
                         currentAnimator = null;
                     }
                 });
@@ -146,7 +167,33 @@ public class MapWijnhaven_103 extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.thumb_button_21:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_min1);
+                break;
+            case R.id.thumb_button_22:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_bg);
+                break;
+            case R.id.thumb_button_23:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_1);
+                break;
+            case R.id.thumb_button_24:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_2);
+                break;
+            case R.id.thumb_button_25:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_3);
+                break;
+            case R.id.thumb_button_26:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_4);
+                break;
+            case R.id.thumb_button_27:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_5);
+                break;
+            case R.id.thumb_button_28:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_6);
+                break;
+            case R.id.thumb_button_29:
+                zoomImageFromThumb(v, R.drawable.wijnhaven_103_dak);
+                break;
         }
     }
 }
