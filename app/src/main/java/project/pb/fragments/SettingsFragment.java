74d5f8764bac04843c6c41ce1c7dfa66bc5647a7
FurkanCalikHR <0928212@hr.nl>
2019-06-03 13:37:50 +0200

    Settings page dark mode applied

--- app/src/main/java/project/pb/fragments/SettingsFragment.java
+++ app/src/main/java/project/pb/fragments/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,38 +20,40 @@ public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        sharedPref = new SharedPref(getContext().getApplicationContext());
+        if(sharedPref.loadNightModeState() == true) {
+            getActivity().setTheme(R.style.DarkTheme);
+        } else {
+            getActivity().setTheme(R.style.AppTheme);
+        }
         View view = inflater.inflate(R.layout.settings_page, container, false);
         getActivity().setTitle("Settings");
 
-        sharedPref = new SharedPref(view.getContext());
-
-        if (sharedPref.loadNightModeState()==true) {
-            getActivity().setTheme(R.style.AppTheme);
-        } else {
-            getActivity().setTheme(R.style.DarkTheme);
-        }
-
+        //sharedPref = new SharedPref(getContext().getApplicationContext());
         myswitch = (Switch) view.findViewById(R.id.switch3);
-        if (sharedPref.loadNightModeState()==true) {
+
+        if (sharedPref.loadNightModeState() == true) {
             myswitch.setChecked(true);
         }
         myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     sharedPref.setNightModeState(true);
-                    refreshApplciation();
+                    restart();
+                    //getActivity().recreate();
                 } else {
                     sharedPref.setNightModeState(false);
-                    refreshApplciation();
+                    restart();
+                    //getActivity().recreate();
                 }
             }
         });
         return view;
     }
 
-    public void refreshApplciation() {
+    private void restart() {
         getFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new SettingsFragment()).commit();
     }
-
 }
